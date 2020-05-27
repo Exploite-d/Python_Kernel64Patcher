657dbdbf00afb30832fa7773ef38aabff8d956c9
Exploite-d <51859413+Exploite-d@users.noreply.github.com>
2020-05-27 13:08:19 +0200

    Update Kernel64Patcher.py

--- Kernel64Patcher.py
+++ Kernel64Patcher.py
@@ -33,7 +33,7 @@ if args.amfi11:
 	kernel_file = open("./kernel.inprogress", "r+b")
 	fh = kernel_file
 	file = fh.read()
-	hex_value = hex(file.index(b"\xF8\x5F\xBC\xA9\xF6\x57\x01\xA9\xF4\x4F\x02\xA9\xFD\x7B\x03\xA9\xFD\xC3\x00\x91\xF3\x03\x00\xAA\x15\x00\x80\x52")) #Finding pattern
+	hex_value = hex(file.index(b"\xF8\x5F\xBC\xA9\xF6\x57\x01\xA9\xF4\x4F\x02\xA9\xFD")) #Finding pattern
 	hex_value = int(hex_value, 0)
 	fh.seek(hex_value, 0)
 	fh.write(b"\xE0\x03\x00\x32\xC0\x03\x5F\xD6") #Applying AMFI patch
@@ -43,7 +43,7 @@ if args.amfi12:
 	kernel_file = open("./kernel.inprogress", "r+b")
 	fh = kernel_file
 	file = fh.read()
-	hex_value = hex(file.index(b"\xFD\x7B\xBF\xA9\xFD\x03\x00\x91\x02\x9B\xE6\x97\x1F\x00\x00\x71\xE0\x07\x9F\x1A\xFD\x7B\xC1\xA8\xC0\x03\x5F\xD6")) #Finding pattern
+	hex_value = hex(file.index(b"\xFD\x7B\xBF\xA9\xFD\x03\x00\x91\x02\x9B\xE6\x97\x1F")) #Finding pattern
 	hex_value = int(hex_value, 0)
 	fh.seek(hex_value, 0)
 	fh.write(b"\xE0\x03\x00\x32\xC0\x03\x5F\xD6") #Applying AMFI patch
@@ -53,7 +53,7 @@ if args.amfi13:
 	kernel_file = open("./kernel.inprogress", "r+b")
 	fh = kernel_file
 	file = fh.read()
-	hex_value = hex(file.index(b"\xFD\x7B\xBF\xA9\xFD\x03\x00\x91\xD9\x15\x00\x94\xFD\x7B\xC1\xA8\xC0\x03\x5F\xD6\xFF\xC3\x00\xD1\xF4\x4F\x01\xA9")) #Finding pattern
+	hex_value = hex(file.index(b"\xFD\x7B\xBF\xA9\xFD\x03\x00\x91\xD9\x15\x00\x94\xFD")) #Finding pattern
 	hex_value = int(hex_value, 0)
 	fh.seek(hex_value, 0)
 	fh.write(b"\xE0\x03\x00\x32\xC0\x03\x5F\xD6") #Applying AMFI patch
